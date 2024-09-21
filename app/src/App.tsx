9eee07e0da4d1acf048e2692ec284d45171cdf05
etowahadams <etowahadams@gmail.com>
2024-09-21 18:34:59 -0400

    feat: basic working with real data

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,24 +2,61 @@ import MolstarViewer from "./components/MolstarViewer";
 import SeqViewer from "./components/SeqViewer";
 
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+
+import { data } from "./data";
 
 function App() {
+  const [feature, setFeature] = useState(0);
+
+  return (
+    <div>
+      <aside
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            {data.map((seqInfo, i) => (
+              <li>
+                <a
+                  href="#"
+                  onClick={() => setFeature(i)}
+                  className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                    feature === i ? "font-bold" : ""
+                  }`}
+                >
+                  <span className="ms-3">{seqInfo.dimenstion}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+      <div className="p-4 sm:ml-64">
+        <h1 className="text-3xl font-bold">{data[feature].dimenstion}</h1>
+        <div className="p-4 mt-5 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+          {data[feature].examples.map((seq) => (
+            <SeqViewer seq={seq} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="flex">
         <div className="w-1/4 bg-gray-200">
-          {/* Sidebar content */}
+          {data.map((seqInfo, i) => (
+            <p onClick={() => setFeature(i)}>{seqInfo.feature_name}</p>
+          ))}
         </div>
         <div className="w-3/4">
-          <div className="card">
-            <SeqViewer
-              seq={{
-                tokens_acts_list: [1, 2, 3, 4, 5, 6],
-                tokens_id_list: [1, 2, 3, 4, 5, 6],
-              }}
-            />
-          </div>
+          <h1 className="text-3xl font-bold underline">{feature}</h1>
+          {data[feature].examples.map((seq) => (
+            <SeqViewer seq={seq} />
+          ))}
         </div>
       </div>
     </>
--- app/src/components/SeqViewer.tsx
+++ app/src/components/SeqViewer.tsx
@@ -1,13 +1,14 @@
 import React, { useMemo } from "react";
 
-interface SeqFormat {
-  quantile_name: string;
+export interface SeqFormat {
+  dimension: number;
+  feature_name?: string;
   examples: SingleSeq[];
 }
 
 interface SingleSeq {
   tokens_acts_list: Array<number>;
-  tokens_id_list: Array<number>;
+  tokens_list: Array<number>;
 }
 
 interface SeqViewerProps {
@@ -18,21 +19,50 @@ interface SeqViewerProps {
  * Color map from white to red
  */
 function redColorMap(value: number, maxValue: number) {
-    // Ensure value is between 0 and maxValue
-    value = Math.max(0, Math.min(value, maxValue));
+  // Ensure value is between 0 and maxValue
+  value = Math.max(0, Math.min(value, maxValue));
 
-    // Normalize value between 0 and 1
-    const normalized = value / maxValue;
+  // Normalize value between 0 and 1
+  const normalized = value / maxValue;
 
-    // Interpolate between white (255, 255, 255) and red (255, 0, 0)
-    const red = 255;
-    const green = Math.round(255 * (1 - normalized));
-    const blue = Math.round(255 * (1 - normalized));
+  // Interpolate between white (255, 255, 255) and red (255, 0, 0)
+  const red = 255;
+  const green = Math.round(255 * (1 - normalized));
+  const blue = Math.round(255 * (1 - normalized));
 
-    // Return the interpolated color as a hex string
-    return `rgb(${red}, ${green}, ${blue})`;
+  // Return the interpolated color as a hex string
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
+const token_dict = {
+  4: "L",
+  5: "A",
+  6: "G",
+  7: "V",
+  8: "S",
+  9: "E",
+  10: "R",
+  11: "T",
+  12: "I",
+  13: "D",
+  14: "P",
+  15: "K",
+  16: "Q",
+  17: "N",
+  18: "F",
+  19: "Y",
+  20: "M",
+  21: "H",
+  22: "W",
+  23: "C",
+  24: "X",
+  25: "B",
+  26: "U",
+  27: "Z",
+  28: "O",
+  29: ".",
+  30: "-",
+};
 
 /**
  * This component takes in a SingleSeq and renders the sequence with the corresponding colors
@@ -45,9 +75,19 @@ const SeqViewer: React.FC<SeqViewerProps> = ({ seq }) => {
 
   return (
     <div>
-      {seq.tokens_id_list.map((token_id, index) => {
+      {seq.tokens_list.map((token, index) => {
         const color = redColorMap(seq.tokens_acts_list[index], maxValue);
-        return <span style={{ backgroundColor: color, borderRadius: 2 }}>{token_id}</span>;
+        return (
+          <span
+            style={{
+              backgroundColor: color,
+              borderRadius: 2,
+              letterSpacing: -1,
+            }}
+          >
+            {token_dict[seq.tokens_list[index]]}
+          </span>
+        );
       })}
     </div>
   );
