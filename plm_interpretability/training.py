88ef1355b00f1f8ffd5a57a90c6501752eb9225c
Minji Lee <43370266+haewonc@users.noreply.github.com>
2024-10-17 16:48:29 -0400

    Update training.py

--- plm_interpretability/training.py
+++ plm_interpretability/training.py
@@ -8,24 +8,23 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from sae_module import SAELightningModule 
 from data_module import SequenceDataModule
 
-os.environ["WANDB_CACHE_DIR"] = '/global/cfs/cdirs/m4351/ml5045/wandb'
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--output-dir', type=str, required=True)
-parser.add_argument('--data-dir', type=str, required=True)
-parser.add_argument('--esm2-weight', type=str, default='/global/cfs/cdirs/m4351/ml5045/interp_weights/esm2_t33_650M_UR50D.pt')
+parser.add_argument('--data-dir', type=str, default='data/uniref50_1M_1022.parquet')
+parser.add_argument('--esm2-weight', type=str, default='weights/esm2_t33_650M_UR50D.pt')
 parser.add_argument('-l', '--layer-to_use', type=int, default=24)
 parser.add_argument('--d-model', type=int, default=1280)
 parser.add_argument('--d-hidden', type=int, default=32768)
 parser.add_argument('-b', '--batch-size', type=int, default=64)
 parser.add_argument('--lr', type=float, default=2e-3)
 parser.add_argument('--k', type=int, default=128)
-parser.add_argument('--auxk', type=int, default=256)
 parser.add_argument('--dead-steps-threshold', type=int, default=2000)
 parser.add_argument('-e', '--max-epochs', type=int, default=1)
 parser.add_argument('-d', '--num-devices', type=int, default=1)
 
 args = parser.parse_args()
+args.auxk = args.k * 2
+args.output_dir = f"results_l{args.layer_to_use}_dim{args.d_hidden}_k{args.k}"
 
 if not os.path.exists(args.output_dir):
     os.mkdir(args.output_dir)
@@ -56,7 +55,8 @@ trainer = pl.Trainer(
     strategy='ddp' if args.num_devices > 1 else 'auto',
     logger=wandb_logger,
     log_every_n_steps=10,
-    val_check_interval=10,
+    val_check_interval=2000,
+    limit_val_batches=10,
     callbacks=[checkpoint_callback],
     gradient_clip_val=1.0,
 )
