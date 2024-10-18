3614909a483ed320bf30ce266dd7f6f5681115d2
etowahadams <etowahadams@gmail.com>
2024-10-18 17:17:06 -0400

    fix: pull out esm_model

--- plm_interpretability/sae_module.py
+++ plm_interpretability/sae_module.py
@@ -2,9 +2,26 @@ import esm
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
+from functools import cache
 from esm_wrapper import ESM2Model
 from sae_model import SparseAutoencoder, loss_fn
 
+
+@cache
+def get_esm_model(d_model, alphabet, esm2_weight):
+    esm2_model = ESM2Model(
+            num_layers=33,
+            embed_dim=d_model,
+            attention_heads=20,
+            alphabet=alphabet,
+            token_dropout=False,
+        )
+    esm2_model.load_esm_ckpt(esm2_weight)
+    esm2_model.eval()
+    for param in esm2_model.parameters():
+        param.requires_grad = False
+    
+    return esm2_model
 
 class SAELightningModule(pl.LightningModule):
     def __init__(self, args):
@@ -21,18 +38,6 @@ class SAELightningModule(pl.LightningModule):
             dead_steps_threshold=args.dead_steps_threshold,
         )
         self.alphabet = esm.data.Alphabet.from_architecture("ESM-1b")
-        esm2_model = ESM2Model(
-            num_layers=33,
-            embed_dim=args.d_model,
-            attention_heads=20,
-            alphabet=self.alphabet,
-            token_dropout=False,
-        )
-        esm2_model.load_esm_ckpt(args.esm2_weight)
-        self.esm2_model = esm2_model
-        self.esm2_model.eval()
-        for param in self.esm2_model.parameters():
-            param.requires_grad = False
 
     def forward(self, x):
         return self.sae_model(x)
@@ -41,7 +46,8 @@ class SAELightningModule(pl.LightningModule):
         seqs = batch["Sequence"]
         batch_size = len(seqs)
         with torch.no_grad():
-            tokens, esm_layer_acts = self.esm2_model.get_layer_activations(seqs, self.layer_to_use)
+            esm2_model = get_esm_model(self.args.d_model, self.alphabet, self.args.esm2_weight)
+            tokens, esm_layer_acts = esm2_model.get_layer_activations(seqs, self.layer_to_use)
         recons, auxk, num_dead = self(esm_layer_acts)
         mse_loss, auxk_loss = loss_fn(esm_layer_acts, recons, auxk)
         loss = mse_loss + auxk_loss
@@ -84,11 +90,12 @@ class SAELightningModule(pl.LightningModule):
         seqs = batch["Sequence"]
         batch_size = len(seqs)
         with torch.no_grad():
-            tokens, esm_layer_acts = self.esm2_model.get_layer_activations(seqs, self.layer_to_use)
+            esm2_model = get_esm_model(self.args.d_model, self.alphabet, self.args.esm2_weight)
+            tokens, esm_layer_acts = esm2_model.get_layer_activations(seqs, self.layer_to_use)
             recons, auxk, num_dead = self(esm_layer_acts)
             mse_loss, auxk_loss = loss_fn(esm_layer_acts, recons, auxk)
             loss = mse_loss + auxk_loss
-            logits = self.esm2_model.get_sequence(recons, self.layer_to_use)
+            logits = esm2_model.get_sequence(recons, self.layer_to_use)
             logits = logits.view(-1, logits.size(-1))
             tokens = tokens.view(-1)
             correct = (torch.argmax(logits, dim=-1) == tokens).sum().item()
