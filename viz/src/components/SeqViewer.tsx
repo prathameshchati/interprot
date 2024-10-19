0bed0736accd4a177801cefc98e84a04413d0fb5
etowahadams <etowahadams@gmail.com>
2024-10-19 17:29:18 -0400

    feat: hide non-zero sequences

--- viz/src/components/SeqViewer.tsx
+++ viz/src/components/SeqViewer.tsx
@@ -65,6 +65,15 @@ const token_dict: { [key: number]: string } = {
   30: "-",
 };
 
+function getFirstNonZeroIndex(arr: Array<number>) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      return i;
+    }
+  }
+  return 0;
+}
+
 /**
  * This component takes in a SingleSeq and renders the sequence with the corresponding colors
  */
@@ -72,11 +81,28 @@ const SeqViewer: React.FC<SeqViewerProps> = ({ seq }) => {
   const maxValue = useMemo(() => {
     return Math.max(...seq.tokens_acts_list);
   }, [seq.tokens_acts_list]);
+  const firstNonZero = getFirstNonZeroIndex(seq.tokens_acts_list);
+
+  const startIdx = Math.max(firstNonZero - 30, 0);
+
+  const tokensToShow = seq.tokens_list.slice(startIdx);
+  const activationsToShow = seq.tokens_acts_list.slice(startIdx);
 
   return (
-    <div>
-      {seq.tokens_list.map((token, index) => {
-        const color = redColorMap(seq.tokens_acts_list[index], maxValue);
+    <div className="inline-flex">
+      {startIdx > 0 && (
+        <span className="relative inline-flex group">
+          <span style={{ color: "gray" }}>+{startIdx}</span>
+          ...
+          {/* Tooltip */}
+          <span className="invisible absolute left-full ml-2 top-1/2 -translate-y-1/2 w-max bg-gray-900 text-white text-xs rounded-md px-2 py-1 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity">
+            The number of amino acids hidden in this sequence. (AlphaFoldDB ID:{" "}
+            {seq.alphafold_id})
+          </span>
+        </span>
+      )}
+      {tokensToShow.map((token, index) => {
+        const color = redColorMap(activationsToShow[index], maxValue);
         return (
           <span
             key={`token-${index}`}
