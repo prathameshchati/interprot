0725c6aab422f398ceb698c22859b89c9bf62467
Liam <liambai2000@gmail.com>
2024-10-20 23:38:07 -0400

    Pass auxk and enforce checkpoing naming convention

--- plm_interpretability/training.py
+++ plm_interpretability/training.py
@@ -18,38 +18,36 @@ parser.add_argument("--d-hidden", type=int, default=32768)
 parser.add_argument("-b", "--batch-size", type=int, default=4)
 parser.add_argument("--lr", type=float, default=2e-4)
 parser.add_argument("--k", type=int, default=128)
+parser.add_argument("--auxk", type=int, default=256)
 parser.add_argument("--dead-steps-threshold", type=int, default=2000)
 parser.add_argument("-e", "--max-epochs", type=int, default=4)
 parser.add_argument("-d", "--num-devices", type=int, default=1)
 
 args = parser.parse_args()
-args.auxk = args.k * 2
 args.output_dir = f"results_l{args.layer_to_use}_dim{args.d_hidden}_k{args.k}"
 
 if not os.path.exists(args.output_dir):
     os.mkdir(args.output_dir)
 
+sae_name = f"esm2_plm1280_l{args.layer_to_use}_sae{args.d_hidden}_k{args.k}_auxk{args.auxk}"
 wandb_logger = WandbLogger(
     project="interpretability",
-    name=f"sae_train_l{args.layer_to_use}_dim{args.d_hidden}_k{args.k}",
+    name=sae_name,
     save_dir=os.path.join(args.output_dir, "wandb"),
 )
 
-# Initialize model and data module
 model = SAELightningModule(args)
 data_module = SequenceDataModule(args.data_dir, args.batch_size)
 
-# Set up callbacks
 checkpoint_callback = ModelCheckpoint(
     dirpath=os.path.join(args.output_dir, "checkpoints"),
-    filename="sae-{step}-{val_loss:.2f}",
+    filename=sae_name + "-{step}-{val_loss:.2f}",
     save_top_k=3,
     monitor="val_loss",
     mode="min",
     save_last=True,
 )
 
-# Initialize Trainer
 trainer = pl.Trainer(
     max_epochs=args.max_epochs,
     accelerator="gpu",
@@ -63,11 +61,7 @@ trainer = pl.Trainer(
     gradient_clip_val=1.0,
 )
 
-# Train the model
 trainer.fit(model, data_module)
-
-# Test the model
 trainer.test(model, data_module)
 
-# Close WandB run
 wandb.finish()
