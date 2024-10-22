e6d401f68939280df7f071fce02aeb098dbc8fba
Liam <liambai2000@gmail.com>
2024-10-22 00:03:59 -0400

    Some cleanup and documentation of make_viz_files module

--- plm_interpretability/make_viz_files/__main__.py
+++ plm_interpretability/make_viz_files/__main__.py
@@ -2,7 +2,6 @@ import heapq
 import json
 import os
 import re
-from functools import lru_cache
 from typing import Any
 
 import click
@@ -32,7 +31,6 @@ class TopKHeap:
         return sorted(self.heap, reverse=True)
 
 
-@lru_cache(maxsize=100000)
 def get_sae_acts(
     seq: str,
     tokenizer: AutoTokenizer,
@@ -64,7 +62,18 @@ def get_sae_acts(
     help="Path to the sequences file containing AlphaFoldDB IDs",
 )
 def make_viz_files(checkpoint_file: str, sequences_file: str):
-    """Generate visualization files for SAE latents"""
+    """
+    Generate visualization files for SAE latents. Writes a JSON file of the format
+    [
+        {
+            "tokens_acts_list": [0, 0, 0, 1, 1],
+            "tokens_list": [1, 2, 3, 4, 5],
+            "alphafold_id": "AF-A0A4W3JAN5-F1-v4",
+        },
+        ...
+    ]
+    for each hidden dimension where each entry corresponds to a highly activated sequence.
+    """
     click.echo(f"Generating visualization files for {checkpoint_file}")
 
     pattern = r"plm(\d+).*?l(\d+).*?sae(\d+)"
@@ -90,6 +99,7 @@ def make_viz_files(checkpoint_file: str, sequences_file: str):
             }
         )
 
+    # Build a heap for each hidden dimension to store the top NUM_SEQS_PER_DIM sequences
     hidden_dim_to_seqs = {dim: TopKHeap(k=NUM_SEQS_PER_DIM) for dim in range(sae_dim)}
 
     df = pl.read_parquet(sequences_file)
@@ -100,6 +110,7 @@ def make_viz_files(checkpoint_file: str, sequences_file: str):
         sae_acts = get_sae_acts(seq, tokenizer, plm_model, sae_model, plm_layer)
         for dim in range(sae_dim):
             sae_dim_acts = sae_acts[:, dim]
+            # Use the max activation across the sequence for ranking
             max_act = np.max(sae_dim_acts)
             hidden_dim_to_seqs[dim].push((max_act, seq_idx, sae_dim_acts))
 
