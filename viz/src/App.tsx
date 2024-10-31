ce3b6997e48b9abe777b2e3ea501e58e26f96387
Liam <liambai2000@gmail.com>
2024-10-31 13:12:09 -0400

    Add error boundary

--- viz/src/App.tsx
+++ viz/src/App.tsx
@@ -2,15 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SAEVisualizer from "./SAEVisualizer";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/sae-visualizer" element={<SAEVisualizer />} />
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sae-visualizer" element={<SAEVisualizer />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
